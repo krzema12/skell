b65554085be321dbf45cb5ffb4b3426e892c6c5b
Piotr Krzeminski <git@krzeminski.it>
2019-07-03 20:43:33 +0200

    Implenent simple reading from text file

--- src/main/kotlin/it/krzeminski/skell/commands/sequenceRedirection.kt
+++ src/main/kotlin/it/krzeminski/skell/commands/sequenceRedirection.kt
@@ -3,6 +3,7 @@ package it.krzeminski.skell.commands
 import it.krzeminski.skell.SkellContext
 import java.io.File
 import java.io.PrintWriter
+import java.nio.file.Files
 
 infix fun Iterable<String>.writeTo(file: File) {
     val writer = PrintWriter(file, "UTF-8")
@@ -11,5 +12,8 @@ infix fun Iterable<String>.writeTo(file: File) {
     }
     writer.close()
 }
+
+fun readFrom(file: File): Iterable<String> =
+    Files.readAllLines(file.toPath())
 
 fun SkellContext.file(path: String): File = currentPath.resolve(path).toFile()
--- src/test/kotlin/it/krzeminski/skell/commands/SequenceRedirectionTest.kt
+++ src/test/kotlin/it/krzeminski/skell/commands/SequenceRedirectionTest.kt
@@ -2,6 +2,7 @@ package it.krzeminski.skell.commands
 
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
+import java.io.PrintWriter
 import java.nio.file.Files
 
 class SequenceRedirectionTest : StringSpec({
@@ -13,5 +14,19 @@ class SequenceRedirectionTest : StringSpec({
 
         val actualReadLines = String(Files.readAllBytes(tempDirectoryPath.resolve("new-file")))
         actualReadLines shouldBe "foo\nbar\n"
+    }
+
+    "reads text from file" {
+        val tempDirectoryPath = Files.createTempDirectory("unit-tests")
+
+        val writer = PrintWriter(tempDirectoryPath.resolve("new-file").toFile(), "UTF-8")
+        writer.println("foo")
+        writer.println("bar")
+        writer.close()
+
+        cd(tempDirectoryPath.toAbsolutePath().toString()) {
+            val actualReadLines = readFrom(file("new-file"))
+            actualReadLines shouldBe listOf("foo", "bar")
+        }
     }
 })
